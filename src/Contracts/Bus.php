eec4d76b98f7da589fa3f586b0273266e217d141
inxilpro <inxilpro@users.noreply.github.com>
2023-05-06 16:39:21 +0000

    Fix styling

--- src/Contracts/Bus.php
+++ src/Contracts/Bus.php
@@ -6,9 +6,9 @@ use Thunk\Verbs\Event;
 
 interface Bus
 {
-	public function registerListener(object $listener): void;
-	
-	public function dispatch(Event $event): void;
-	
-	public function replay(Event $event): void;
+    public function registerListener(object $listener): void;
+
+    public function dispatch(Event $event): void;
+
+    public function replay(Event $event): void;
 }
--- src/Contracts/Store.php
+++ src/Contracts/Store.php
@@ -7,7 +7,7 @@ use Thunk\Verbs\Event;
 
 interface Store
 {
-	public function save(Event $event): string;
-	
-	public function get(?array $event_types = null, int $chunk_size = 1000): LazyCollection;
+    public function save(Event $event): string;
+
+    public function get(?array $event_types = null, int $chunk_size = 1000): LazyCollection;
 }
