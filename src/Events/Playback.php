e239c0256fceac7e282118d5eddcb24a8caffd6e
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2023-05-10 12:50:35 -0400

    WIP

---
+++ src/Events/Playback.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Thunk\Verbs\Events;
+
+use Thunk\Verbs\Facades\Store;
+
+class Playback
+{
+    public static function for(string $event_type): static
+    {
+        return app()->make(static::class, ['event_type' => $event_type]);
+    }
+
+    public static function all(): static
+    {
+        return app()->make(static::class);
+    }
+
+    public function __construct(
+        protected Store $store,
+        protected Dispatcher $dispatcher,
+        protected ?string $event_type = null,
+    ) {
+    }
+
+    public function run()
+    {
+        $this->store->replay($this->dispatcher->replay(...), $this->event_type);
+    }
+}
