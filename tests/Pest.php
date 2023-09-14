227ca3aaed793c4fe26d59a2123af22c6f952dfc
Chris Morrell <inxilpro@users.noreply.github.com>
2023-09-13 21:53:54 -0400

    Update Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Finder\Finder;
 use Thunk\Verbs\Tests\TestCase;
 
 Model::unguard();
 
-dump(__DIR__.'/../examples/bank/tests');
-uses(TestCase::class)->in(__DIR__, __DIR__.'/../examples/bank/tests');
+$examples = collect(Finder::create()->directories()->in(__DIR__.'/../examples/')->depth(1)->name('tests'))
+	->map(fn(\Symfony\Component\Finder\SplFileInfo $file) => $file->getRealPath())
+	->values()
+	->all();
+
+uses(TestCase::class)->in(__DIR__, ...$examples);
