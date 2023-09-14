c98f820b4044319b774e3485622c278f800708d3
inxilpro <inxilpro@users.noreply.github.com>
2023-09-14 01:54:16 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,8 +7,8 @@ use Thunk\Verbs\Tests\TestCase;
 Model::unguard();
 
 $examples = collect(Finder::create()->directories()->in(__DIR__.'/../examples/')->depth(1)->name('tests'))
-	->map(fn(\Symfony\Component\Finder\SplFileInfo $file) => $file->getRealPath())
-	->values()
-	->all();
+    ->map(fn (\Symfony\Component\Finder\SplFileInfo $file) => $file->getRealPath())
+    ->values()
+    ->all();
 
 uses(TestCase::class)->in(__DIR__, ...$examples);
