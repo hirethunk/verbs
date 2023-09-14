c30b73d4ecab5dfea563bba5bf30cb6c75835361
DanielCoulbourne <DanielCoulbourne@users.noreply.github.com>
2023-09-14 01:46:01 +0000

    Fix styling

--- examples/Bank/database/migrations/2023_09_13_213109_create_users_table.php
+++ examples/Bank/database/migrations/2023_09_13_213109_create_users_table.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-	public function up(): void
-	{
-		Schema::create('users', function(Blueprint $table) {
-			$table->id();
-			
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+
             $table->string('name');
             $table->string('email');
             $table->string('password');
-			
-			$table->timestamps();
-		});
-	}
+
+            $table->timestamps();
+        });
+    }
 };
--- examples/Bank/src/Models/User.php
+++ examples/Bank/src/Models/User.php
@@ -4,7 +4,6 @@ namespace Thunk\Verbs\Examples\Bank\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Thunk\Verbs\FromState;
 
 class User extends Model
 {
