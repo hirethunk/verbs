2d31c7f9572f63ad6bcbb715b46dd3bedcfe3351
inxilpro <inxilpro@users.noreply.github.com>
2023-09-14 01:32:22 +0000

    Fix styling

--- examples/Bank/database/migrations/2023_09_13_213109_create_users_table.php
+++ examples/Bank/database/migrations/2023_09_13_213109_create_users_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-	public function up(): void
-	{
-		Schema::create('users', function(Blueprint $table) {
-			$table->id();
-			
-			
-			
-			$table->timestamps();
-		});
-	}
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
 };
