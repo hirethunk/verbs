cf100b3d5496a3f7620083c1afb9b939f668a430
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-02 16:11:37 -0400

    Create GainedFood.php

---
+++ examples/Wingspan/src/Events/GainedFood.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Thunk\Verbs\Examples\Wingspan\Events;
+
+use Thunk\Verbs\Attributes\Autodiscovery\AppliesToState;
+use Thunk\Verbs\Event;
+use Thunk\Verbs\Examples\Wingspan\Game\Birds\Bird;
+use Thunk\Verbs\Examples\Wingspan\Game\Food;
+use Thunk\Verbs\Examples\Wingspan\States\PlayerState;
+use Thunk\Verbs\Examples\Wingspan\States\RoundState;
+
+#[AppliesToState(PlayerState::class)]
+class GainedFood extends Event
+{
+    public function __construct(
+        public int $player_id,
+        public Food $food,
+    ) {
+        // TODO: Eventually confirm that this food is available
+    }
+
+    public function applyToPlayer(PlayerState $player)
+    {
+        $player->food->push($this->food);
+        $player->available_action_cubes--;
+    }
+}
