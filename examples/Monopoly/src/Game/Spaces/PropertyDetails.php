44c103158f4784bc74f199b55ee631bd799bad81
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-03 19:15:31 -0400

    WIP

--- examples/Monopoly/src/Game/Spaces/PropertyDetails.php
+++ examples/Monopoly/src/Game/Spaces/PropertyDetails.php
@@ -2,6 +2,36 @@
 
 namespace Thunk\Verbs\Examples\Monopoly\Game\Spaces;
 
+use Brick\Money\Money;
+use Thunk\Verbs\Examples\Monopoly\Game\PropertyColor;
+
 abstract class PropertyDetails extends SpaceDetails
 {
+    public PropertyColor $color;
+
+    public int $price;
+
+    /** @var int[] */
+    public array $rent;
+
+    public int $building_cost;
+
+    public int $development = 0;
+
+    public bool $mortgaged = false;
+
+    public function price(): Money
+    {
+        return Money::of($this->price, 'USD');
+    }
+
+    public function rent(): Money
+    {
+        return Money::of($this->rent[$this->development], 'USD');
+    }
+
+    public function buildingCost(): Money
+    {
+        return Money::of($this->building_cost, 'USD');
+    }
 }
--- examples/Monopoly/src/Game/Spaces/SpaceDetails.php
+++ examples/Monopoly/src/Game/Spaces/SpaceDetails.php
@@ -4,4 +4,7 @@ namespace Thunk\Verbs\Examples\Monopoly\Game\Spaces;
 
 abstract class SpaceDetails
 {
+    public string $name;
+
+    public int $position;
 }
