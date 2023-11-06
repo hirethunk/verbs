7dc69fec2eb7f2eadedbc43d0b5a117d565b1958
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-06 00:07:04 -0500

    Dynamic routes

--- src/VerbsServiceProvider.php
+++ src/VerbsServiceProvider.php
@@ -70,15 +70,5 @@ class VerbsServiceProvider extends PackageServiceProvider
                 $this->app->make(Broker::class)->fire($event);
             }
         });
-
-        // $this->publishes([
-        //     __DIR__.'/../config/verbs.php' => config_path('verbs.php'),
-        // ], 'verbs-config');
-
-        // $this->publishes([
-        //     __DIR__.'/../database/migrations/create_verb_events_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_verb_events_table.php'),
-        //     __DIR__.'/../database/migrations/create_verb_snapshots_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_verb_snapshots_table.php'),
-        //     __DIR__.'/../database/migrations/create_verb_state_events_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_verb_state_events_table.php'),
-        // ], 'verbs-migrations');
     }
 }
