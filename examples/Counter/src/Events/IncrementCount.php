5cb5b78a4254772f2e916c2c084507b19afae868
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-09 15:46:32 -0500

    Update IncrementCount.php

--- examples/Counter/src/Events/IncrementCount.php
+++ examples/Counter/src/Events/IncrementCount.php
@@ -7,7 +7,15 @@ use Thunk\Verbs\Event;
 use Thunk\Verbs\Examples\Counter\States\CountState;
 
 /**
- * This is a very **basic** example.
+ * In our most basic example of event sourcing, we just use a single event
+ * to increment a counter. This event uses `AppliesToSingletonState` to tell
+ * Verbs that it should always be applied to a single `CountState` across the
+ * entire application (as opposed to having different counts for different
+ * situations).
+ *
+ * Because we're using a singleton state, there is no need for the event to
+ * have a `$count_id`.
+ *
  */
 #[AppliesToSingletonState(CountState::class)]
 class IncrementCount extends Event
