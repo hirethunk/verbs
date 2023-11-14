db49ac0f31bbfc1e769b3980a84ea0fade1a98be
inxilpro <inxilpro@users.noreply.github.com>
2023-11-14 19:20:52 +0000

    Fix styling

--- tests/Unit/StateManagerTest.php
+++ tests/Unit/StateManagerTest.php
@@ -6,20 +6,21 @@ use Thunk\Verbs\Lifecycle\SnapshotStore;
 use Thunk\Verbs\Lifecycle\StateManager;
 use Thunk\Verbs\State;
 
-beforeEach(function() {
-	$this->dispatcher = Mockery::mock(Dispatcher::class);
-	$this->snapshots = Mockery::mock(SnapshotStore::class);
-	$this->events = Mockery::mock(EventStore::class);
-	$this->manager = new StateManager($this->dispatcher, $this->snapshots, $this->events);
+beforeEach(function () {
+    $this->dispatcher = Mockery::mock(Dispatcher::class);
+    $this->snapshots = Mockery::mock(SnapshotStore::class);
+    $this->events = Mockery::mock(EventStore::class);
+    $this->manager = new StateManager($this->dispatcher, $this->snapshots, $this->events);
 });
 
-it('it remembers state', function() {
-	$state = new class extends State {
-	};
-	
-	$this->manager->register($state);
-	
-	expect($state->id)->toBeInt();
-	
-	expect($this->manager->load($state->id, $state::class))->toBe($state);
+it('it remembers state', function () {
+    $state = new class extends State
+    {
+    };
+
+    $this->manager->register($state);
+
+    expect($state->id)->toBeInt();
+
+    expect($this->manager->load($state->id, $state::class))->toBe($state);
 });
