7fcc943c39464fe6e09d4e0ed3ddff0e44c4acab
Chris Morrell <inxilpro@users.noreply.github.com>
2023-12-07 17:11:17 -0500

    Code style

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -6,8 +6,8 @@ use Thunk\Verbs\Support\PendingEvent;
 if (! function_exists('verb')) {
     function verb(Event $event, bool $commit = false): Event
     {
-		$pending = new PendingEvent($event);
-		
+        $pending = new PendingEvent($event);
+
         return $commit ? $pending->fireAndCommit() : $pending->fire();
     }
 }
