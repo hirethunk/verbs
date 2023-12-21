c90e0401b5274d0872fab7525d58074b41f51425
Chris Morrell <inxilpro@users.noreply.github.com>
2023-12-21 10:58:10 -0500

    Link to docs

--- src/Exceptions/DoNotStoreModelsOnEventsOrStates.php
+++ src/Exceptions/DoNotStoreModelsOnEventsOrStates.php
@@ -12,8 +12,8 @@ class DoNotStoreModelsOnEventsOrStates extends RuntimeException
     {
         $type = class_basename($object);
 
-        // TODO: Write docs page and link to it
+        $docs_url = 'https://verbs.thunk.dev/docs/crash-course/state-vs-model#content-dont-mix-models-and-states';
 
-        parent::__construct("You are trying to store a '{$type}' in your Verbs event data. This is probably not what you want to do!");
+        parent::__construct("You are trying to store a '{$type}' in your Verbs event data. This is probably not what you want to do! See: <{$docs_url}>");
     }
 }
