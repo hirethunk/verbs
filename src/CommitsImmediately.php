7a8fdf4b1c2708a64696d7e3b7c483ede8fc21d6
inxilpro <inxilpro@users.noreply.github.com>
2023-12-29 16:51:16 +0000

    Fix styling

--- src/CommitsImmediately.php
+++ src/CommitsImmediately.php
@@ -2,9 +2,6 @@
 
 namespace Thunk\Verbs;
 
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-
 interface CommitsImmediately
 {
 }
