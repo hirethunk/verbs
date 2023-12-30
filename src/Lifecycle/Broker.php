6251c6f228ffe1f188ad0a14a3b0ea9f5989ea23
Chris Morrell <inxilpro@users.noreply.github.com>
2023-12-29 20:42:28 -0500

    New way to access created_at

--- src/Lifecycle/Broker.php
+++ src/Lifecycle/Broker.php
@@ -80,10 +80,12 @@ class Broker
                     $beforeEach($event);
                 }
 
+                $created_at = app(MetadataManager::class)->getEphemeral($event, 'created_at', now());
+
                 $event->states()
                     ->each(fn ($state) => $this->dispatcher->apply($event, $state))
-                    ->each(fn ($state) => $this->dispatcher->replay($event, $state, $model->created_at)) // FIXME
-                    ->whenEmpty(fn () => $this->dispatcher->replay($event, null, $model->created_at)); // FIXME
+                    ->each(fn ($state) => $this->dispatcher->replay($event, $state, $created_at))
+                    ->whenEmpty(fn () => $this->dispatcher->replay($event, null, $created_at));
 
                 if ($afterEach) {
                     $afterEach($event);
