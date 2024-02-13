90c58ca9b0a8a57dd5fcb4bada81e17260e5b066
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2024-02-12 23:30:25 -0500

    beef up state factory

--- src/StateFactory.php
+++ src/StateFactory.php
@@ -8,14 +8,21 @@ class StateFactory
 {
     protected ?int $id = null;
 
+    protected array $data = [];
+
+    protected $state;
+
     public function __construct(
         protected string $state_class
     ) {
+        $this->state = new $state_class;
     }
 
     public function for(int $id): static
     {
         $this->id = $id;
+
+        $this->state = $this->state_class::load($id);
 
         return $this;
     }
@@ -25,7 +32,7 @@ class StateFactory
         return VerbsStateInitialized::fire(
             state_id: $id ?? $this->id,
             state_class: $this->state_class,
-            state_data: $data
+            state_data: array_merge($this->data, $data),
         )->state();
     }
 }
