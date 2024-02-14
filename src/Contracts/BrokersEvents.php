f6086771ac19dba360e35e57dc12a0b791032f4f
Chris Morrell <inxilpro@users.noreply.github.com>
2024-02-14 17:06:28 -0500

    Remove some convenience methods from interface

--- src/Contracts/BrokersEvents.php
+++ src/Contracts/BrokersEvents.php
@@ -2,9 +2,6 @@
 
 namespace Thunk\Verbs\Contracts;
 
-use Glhd\Bits\Bits;
-use Ramsey\Uuid\UuidInterface;
-use Symfony\Component\Uid\AbstractUid;
 use Thunk\Verbs\Event;
 
 interface BrokersEvents
@@ -18,14 +15,4 @@ interface BrokersEvents
     public function isAllowed(Event $event): bool;
 
     public function replay(?callable $beforeEach = null, ?callable $afterEach = null);
-
-    public function isReplaying(): bool;
-
-    public function unlessReplaying(callable $callback);
-
-    public function createMetadataUsing(?callable $callback = null): void;
-
-    public function toId(Bits|UuidInterface|AbstractUid|int|string|null $id): int|string|null;
-
-    public function commitImmediately(bool $commit_immediately = true): void;
 }
