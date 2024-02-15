3b9fa31d03b8c7c123aa14392d2a39685eb04074
Chris Morrell <inxilpro@users.noreply.github.com>
2024-02-15 16:32:50 -0500

    Revert "Revert "WIP""
    This reverts commit ba7170e6f4309eeccd31e750f546b2fb90299961.

--- database/migrations/create_verb_state_events_table.php
+++ database/migrations/create_verb_state_events_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Thunk\Verbs\Support\IdManager;
 
 return new class extends Migration
 {
@@ -15,7 +16,7 @@ return new class extends Migration
 
             // The 'state_id' column needs to be set up differently depending
             // on if you're using Snowflakes vs. ULIDs/etc.
-            $this->createConfiguredStateIdType($table);
+            app(IdManager::class)->createColumnDefinition($table, 'state_id')->index();
 
             $table->string('state_type')->index();
 
@@ -26,17 +27,5 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('verb_state_events');
-    }
-
-    protected function createConfiguredStateIdType(Blueprint $table)
-    {
-        $id_type = strtolower(config('verbs.id_type', 'snowflake'));
-
-        return match ($id_type) {
-            'snowflake' => $table->snowflake('state_id')->index(),
-            'ulid' => $table->ulid('state_id')->index(),
-            'uuid' => $table->uuid('state_id')->index(),
-            'default' => throw new UnexpectedValueException("Unknown Verbs ID type: '{$id_type}'"),
-        };
     }
 };
