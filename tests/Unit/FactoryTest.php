4879d7801a8d4b7ff73997fd1613e2fa1ddc2bf2
Chris Morrell <inxilpro@users.noreply.github.com>
2024-02-15 16:23:56 -0500

    A little testing

--- tests/Unit/FactoryTest.php
+++ tests/Unit/FactoryTest.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Thunk\Verbs\State;
+use Thunk\Verbs\StateFactory;
 
 test('a factory can create a state', function () {
-    $state = StateWithId::factory()->create([
+    $state = FactoryTestState::factory()->create([
         'name' => 'daniel',
     ]);
 
@@ -11,13 +13,13 @@ test('a factory can create a state', function () {
 });
 
 test('a factory without parameters can create a state', function () {
-    $state = StateWithId::factory()->create();
+    $state = FactoryTestState::factory()->create();
 
-    expect($state)->toBeInstanceOf(StateWithId::class);
+    expect($state)->toBeInstanceOf(FactoryTestState::class);
 });
 
 test('a factory can accept an id using the create method', function () {
-    $state = StateWithId::factory()->create([
+    $state = FactoryTestState::factory()->create([
         'name' => 'daniel',
     ], 1);
 
@@ -26,7 +28,7 @@ test('a factory can accept an id using the create method', function () {
 });
 
 test('a factory can accept an id using for method', function () {
-    $state = StateWithId::factory()->id(1)->create([
+    $state = FactoryTestState::factory()->id(1)->create([
         'name' => 'daniel',
     ]);
 
@@ -35,7 +37,7 @@ test('a factory can accept an id using for method', function () {
 });
 
 test('a factory can accept an id using the create method over the for method', function () {
-    $state = StateWithId::factory()->id(1)->create([
+    $state = FactoryTestState::factory()->id(1)->create([
         'name' => 'daniel',
     ], 2);
 
@@ -43,7 +45,58 @@ test('a factory can accept an id using the create method over the for method', f
     expect($state->name)->toBe('daniel');
 });
 
-class StateWithId extends State
+test('a custom factory can have a default definition', function () {
+    expect(CustomFactoryTestState::factory()->create()->name)->toBe('Chris');
+});
+
+test('a custom factory can have an array transformation', function () {
+    expect(CustomFactoryTestState::factory()->daniel()->create()->name)->toBe('Daniel');
+});
+
+test('a custom factory can have a callback transformation', function () {
+    expect(CustomFactoryTestState::factory()->john()->create()->name)->toBe('John');
+});
+
+test('multiple states can be factoried at once', function () {
+    $states = CustomFactoryTestState::factory()->count(3)->create();
+
+    expect($states)->toBeInstanceOf(Collection::class)
+        ->and($states->count())->toBe(3)
+        ->and($states[1]->id)->toBeGreaterThan($states[0]->id)
+        ->and($states[2]->id)->toBeGreaterThan($states[1]->id);
+});
+
+class FactoryTestState extends State
 {
     public string $name;
+}
+
+class CustomFactoryTestState extends State
+{
+    public string $name;
+
+    protected static function newFactory(Collection $transformations, ?int $count)
+    {
+        return new class(state_class: static::class, transformations: $transformations, count: $count) extends StateFactory
+        {
+            public function definition(): array
+            {
+                return ['name' => 'Chris'];
+            }
+
+            public function daniel(): static
+            {
+                return $this->state(['name' => 'Daniel']);
+            }
+
+            public function john(): static
+            {
+                return $this->state(function (array $data) {
+                    expect($data['name'])->toBe('Chris');
+
+                    return ['name' => 'John'];
+                });
+            }
+        };
+    }
 }
