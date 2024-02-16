52d8bddd098569c7644490f3b702a2bd6f83053f
Chris Morrell <inxilpro@users.noreply.github.com>
2024-02-15 21:03:13 -0500

    Toying with helpers

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -25,8 +25,22 @@ namespace Thunk\Verbs {
 
     use Thunk\Verbs\Support\IdManager;
 
+    use function app;
+    use function verb;
+
     function make_id()
     {
         return app(IdManager::class)->make();
+    }
+
+    /**
+     * @template TEventType
+     *
+     * @param  Event<TEventType>  $event
+     * @return Event<TEventType>
+     */
+    function fire(Event $event, bool $commit = false): Event
+    {
+        return verb($event, $commit);
     }
 }
