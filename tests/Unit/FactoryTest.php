2f96361e663d2e24fb8510362b605f7fc6353b7f
Chris Morrell <inxilpro@users.noreply.github.com>
2024-02-16 12:51:31 -0500

    Improved factories (#66)
    * Revert "Revert "WIP""
    
    This reverts commit ba7170e6f4309eeccd31e750f546b2fb90299961.
    
    * Revert "Revert "A little testing""
    
    This reverts commit 2fc69a8dd56eb0b5c5c3da35d3d282384e96e258.
    
    * Allow singletons
    
    * Fix singleton states
    
    * Toying with helpers
    
    * Move ID manager to separate PR
    
    * Add a bit more ID flexibility
    
    * Update to use IdManager
    
    * Fix styling
    
    * Minor refactor
    
    * Update test to match changes
    
    * Fix styling

--- tests/Unit/FactoryTest.php
+++ tests/Unit/FactoryTest.php
@@ -1,9 +1,12 @@
 <?php
 
+use Illuminate\Support\Collection;
+use Thunk\Verbs\Lifecycle\StateManager;
 use Thunk\Verbs\State;
+use Thunk\Verbs\StateFactory;
 
 test('a factory can create a state', function () {
-    $state = StateWithId::factory()->create([
+    $state = FactoryTestState::factory()->create([
         'name' => 'daniel',
     ]);
 
@@ -11,13 +14,13 @@ test('a factory can create a state', function () {
 });
 
 test('a factory without parameters can create a state', function () {
-    $state = StateWithId::factory()->create();
+    $state = FactoryTestState::factory()->create();
 
-    expect($state)->toBeInstanceOf(StateWithId::class);
+    expect($state)->toBeInstanceOf(FactoryTestState::class);
 });
 
 test('a factory can accept an id using the create method', function () {
-    $state = StateWithId::factory()->create([
+    $state = FactoryTestState::factory()->create([
         'name' => 'daniel',
     ], 1);
 
@@ -26,7 +29,7 @@ test('a factory can accept an id using the create method', function () {
 });
 
 test('a factory can accept an id using for method', function () {
-    $state = StateWithId::factory()->for(1)->create([
+    $state = FactoryTestState::factory()->id(1)->create([
         'name' => 'daniel',
     ]);
 
@@ -35,7 +38,7 @@ test('a factory can accept an id using for method', function () {
 });
 
 test('a factory can accept an id using the create method over the for method', function () {
-    $state = StateWithId::factory()->for(1)->create([
+    $state = FactoryTestState::factory()->id(1)->create([
         'name' => 'daniel',
     ], 2);
 
@@ -43,7 +46,77 @@ test('a factory can accept an id using the create method over the for method', f
     expect($state->name)->toBe('daniel');
 });
 
-class StateWithId extends State
+test('it auto-generates an IDs', function () {
+    $state1 = FactoryTestState::factory()->create();
+    $state2 = FactoryTestState::factory()->create();
+
+    expect($state1->id)->not->toBeNull()
+        ->and($state2->id)->not->toBeNull()
+        ->not->toBe($state1->id);
+});
+
+test('it can create a singleton state', function () {
+    $singleton_state = FactoryTestState::factory()->singleton()->create();
+
+    expect($singleton_state->id)->not->toBeNull();
+
+    $retreived_state = app(StateManager::class)->singleton(FactoryTestState::class);
+
+    expect($retreived_state)->toBe($singleton_state);
+});
+
+test('a custom factory can have a default definition', function () {
+    expect(CustomFactoryTestState::factory()->create()->name)->toBe('Chris');
+});
+
+test('a custom factory can have an array transformation', function () {
+    expect(CustomFactoryTestState::factory()->daniel()->create()->name)->toBe('Daniel');
+});
+
+test('a custom factory can have a callback transformation', function () {
+    expect(CustomFactoryTestState::factory()->john()->create()->name)->toBe('John');
+});
+
+test('multiple states can be factoried at once', function () {
+    $states = CustomFactoryTestState::factory()->count(3)->create();
+
+    expect($states)->toBeInstanceOf(Collection::class)
+        ->and($states->count())->toBe(3)
+        ->and($states[1]->id)->toBeGreaterThan($states[0]->id)
+        ->and($states[2]->id)->toBeGreaterThan($states[1]->id);
+});
+
+class FactoryTestState extends State
 {
     public string $name;
+}
+
+class CustomFactoryTestState extends State
+{
+    public string $name;
+
+    protected static function newFactory(): StateFactory
+    {
+        return new class(state_class: static::class) extends StateFactory
+        {
+            public function definition(): array
+            {
+                return ['name' => 'Chris'];
+            }
+
+            public function daniel(): static
+            {
+                return $this->state(['name' => 'Daniel']);
+            }
+
+            public function john(): static
+            {
+                return $this->state(function (array $data) {
+                    expect($data['name'])->toBe('Chris');
+
+                    return ['name' => 'John'];
+                });
+            }
+        };
+    }
 }
