fa02d68e9787c5cfd493469826979853e0c540c3
Marc Beinder <marc@marcbeinder.com>
2024-02-26 16:01:55 -0600

    Added Test Cases for Customized Table Names

--- tests/Unit/ConfigurableTableNamesTest.php
+++ tests/Unit/ConfigurableTableNamesTest.php
@@ -1,11 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Thunk\Verbs\Models\VerbEvent;
 use Thunk\Verbs\Models\VerbSnapshot;
 use Thunk\Verbs\Models\VerbStateEvent;
 
 test('VerbEvent table name can be configured', function () {
     $expected_table_name = 'verb_events';
+
+    $verb_model = new VerbEvent();
+    $actual_table_name = $verb_model->getTable();
+
+    expect($expected_table_name)->toBe($actual_table_name);
+});
+
+test('VerbEvent table name can be configured with different table name', function () {
+    $expected_table_name = 'sys_verb_events';
+
+    config()->set('verbs.tables.verb_events', $expected_table_name);
 
     $verb_model = new VerbEvent();
     $actual_table_name = $verb_model->getTable();
@@ -22,8 +34,30 @@ test('VerbSnapshot table name can be configured', function () {
     expect($expected_table_name)->toBe($actual_table_name);
 });
 
+test('VerbSnapshot table name can be configured with different table name', function () {
+    $expected_table_name = 'sys_verb_snapshots';
+
+    config(['verbs.tables.verb_snapshots' => $expected_table_name]);
+
+    $verb_model = new VerbSnapshot();
+    $actual_table_name = $verb_model->getTable();
+
+    expect($expected_table_name)->toBe($actual_table_name);
+});
+
 test('VerbStateEvent table name can be configured', function () {
     $expected_table_name = 'verb_state_events';
+
+    $verb_model = new VerbStateEvent();
+    $actual_table_name = $verb_model->getTable();
+
+    expect($expected_table_name)->toBe($actual_table_name);
+});
+
+test('VerbStateEvent table name can be configured with different table name', function () {
+    $expected_table_name = 'sys_verb_state_events';
+
+    config(['verbs.tables.verb_state_events' => $expected_table_name]);
 
     $verb_model = new VerbStateEvent();
     $actual_table_name = $verb_model->getTable();
