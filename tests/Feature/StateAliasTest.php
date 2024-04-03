dbc49b6266c3a7daf9bb84920c6c75eb7d50d50e
Jacob Davis <jacobamdavis@gmail.com>
2024-04-03 19:28:08 -0400

    failing test

---
+++ tests/Feature/StateAliasTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Thunk\Verbs\Event;
+use Thunk\Verbs\State;
+use Thunk\Verbs\Facades\Verbs;
+use Thunk\Verbs\Attributes\Autodiscovery\StateId;
+
+class TestState extends State {}
+
+class TestEvent extends Event
+{
+    #[StateId(TestState::class, 'foo')]
+    public int $bar;
+}
+
+it('stateid attribute allows setting an alias for a state collection with a single state', function () {
+    $event = TestEvent::fire(bar: 1);
+
+    Verbs::commit();
+
+    $state_collection = $event->states();
+
+    expect($state_collection->aliasNames())->toContain('bar'); // the prop is the alias
+    expect($state_collection->aliasNames())->toContain('foo'); // the string is not
+});
