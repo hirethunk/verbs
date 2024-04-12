033caa2fde56516a2dac1b897ffcc0ed6d357a90
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2024-04-12 17:48:10 -0400

    Add On hook

---
+++ src/Attributes/Hooks/On.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Thunk\Verbs\Attributes\Hooks;
+
+use Attribute;
+use Thunk\Verbs\Lifecycle\Hook;
+use Thunk\Verbs\Lifecycle\Phase;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class On implements HookAttribute
+{
+    public function __construct(
+        public Phase $phase
+    ) { }
+
+    public function applyToHook(Hook $hook): void
+    {
+        $hook->forcePhases($this->phase);
+    }
+}
