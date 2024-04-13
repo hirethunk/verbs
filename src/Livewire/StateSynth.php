155188f9fe1b9c10dcfffd180cecefdd2073cda9
Josh Hanley <josh@hitsystems.com.au>
2024-04-13 15:32:23 +1000

    wip

--- src/Livewire/StateSynth.php
+++ src/Livewire/StateSynth.php
@@ -2,6 +2,7 @@
 
 namespace Thunk\Verbs\Livewire;
 
+use Illuminate\Support\Str;
 use Livewire\Drawer\Utils;
 use Livewire\Mechanisms\HandleComponents\Synthesizers\Synth;
 use Thunk\Verbs\State;
@@ -21,14 +22,35 @@ class StateSynth extends Synth
 
     public function dehydrate($target)
     {
+        $targetClass = $target::class;
+
         $data = Utils::getPublicProperties($target, function (\ReflectionProperty $property) {
             return ! in_array($property->getName(), $this->hidden_properties);
         });
 
         $meta = [
             'id' => $target->id,
-            'type' => get_class($target),
+            'type' => $targetClass,
         ];
+
+        $this->getPublicMethodsDefinedBySubClass($target)
+            ->each(
+                function ($method) use ($target, $targetClass, &$data) {
+                    $attribute = $this->getMethodAttribute($target, $method, \Thunk\Verbs\Livewire\Dehydrate::class);
+
+                    if (is_null($attribute)) {
+                        return;
+                    }
+
+                    $attribute->name = Str::snake($method);
+
+                    if (isset($data[$attribute->getAlias()])) {
+                        throw new \Exception("Cannot dehydrate method `{$method}()` on `{$targetClass}` because a property with name `{$attribute->getAlias()}` already exists. Set a different alias using the `Dehydrate` attribute.");
+                    }
+
+                    $data[$attribute->getAlias()] = $target->{$method}();
+                }
+            );
 
         return [$data, $meta];
     }
@@ -51,5 +73,40 @@ class StateSynth extends Synth
     public function call(&$target, $method, $params, $addEffect)
     {
         throw new \Exception('Cannot call state methods directly.');
+    }
+
+    public function getPublicMethodsDefinedBySubClass($target)
+    {
+        $methods = array_filter((new \ReflectionObject($target))->getMethods(), function ($method) {
+            $isInBaseComponentClass = $method->getDeclaringClass()->getName() === \Thunk\Verbs\State::class;
+
+            return $method->isPublic()
+                && ! $method->isStatic()
+                && ! $isInBaseComponentClass;
+        });
+
+        return collect(array_map(function ($method) {
+            return $method->getName();
+        }, $methods));
+    }
+
+    public function getMethodAttribute($target, $method, $attributeClass)
+    {
+        $method = $this->getMethod($target, $method);
+
+        foreach ($method->getAttributes() as $attribute) {
+            $instance = $attribute->newInstance();
+
+            if ($instance instanceof $attributeClass) {
+                return $instance;
+            }
+        }
+
+        return null;
+    }
+
+    public function getMethod($target, $method)
+    {
+        return (new \ReflectionObject($target))->getMethod($method);
     }
 }
