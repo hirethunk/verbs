7676eaaa324ccd95f48bbff6c6d1b9854c60360e
Josh Hanley <josh@hitsystems.com.au>
2024-04-29 21:32:54 +1000

    wip

--- src/Lifecycle/Standalone/StandaloneBroker.php
+++ src/Lifecycle/Standalone/StandaloneBroker.php
@@ -3,17 +3,19 @@
 namespace Thunk\Verbs\Lifecycle\Standalone;
 
 use Thunk\Verbs\Contracts\BrokersEvents;
+use Thunk\Verbs\Facades\Verbs;
 use Thunk\Verbs\Lifecycle\Broker;
+use Thunk\Verbs\Lifecycle\Phase;
 
 class StandaloneBroker extends Broker implements BrokersEvents
 {
     // @todo - this will need to override commit() to push the events up to the primary broker
-    // public function commit(): bool
-    // {
-    //     Verbs::shouldSkipHooks(Hook::HANDLE);
+    public function commit(): bool
+    {
+        Verbs::skipPhases(Phase::Handle);
 
-    //     parent::commit();
-    // }
+        return parent::commit();
+    }
 
     public function commitToDefault()
     {
