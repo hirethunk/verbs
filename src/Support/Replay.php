282856c56a8242c5e13e387c70e639ce876d6b90
Chris Morrell <inxilpro@users.noreply.github.com>
2025-07-08 12:05:25 -0400

    wip

---
+++ src/Support/Replay.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Thunk\Verbs\Support;
+
+use Illuminate\Support\Enumerable;
+use Thunk\Verbs\Lifecycle\Lifecycle;
+use Thunk\Verbs\Lifecycle\Phases;
+use Thunk\Verbs\State\StateManager;
+
+class Replay
+{
+    public function __construct(
+        public StateManager $states,
+        public Enumerable $events,
+        public Phases $phases,
+    ) {}
+
+    public function handle(): static
+    {
+        $global_registry = app(StateManager::class);
+
+        try {
+            app()->instance(StateManager::class, $this->states);
+
+            foreach ($this->events as $event) {
+                Lifecycle::run($event, $this->phases);
+            }
+        } finally {
+            app()->instance(StateManager::class, $global_registry);
+        }
+
+        return $this;
+    }
+}
